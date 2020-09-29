f972928b5d8fd752ada1b9e0e40961b81ff8ac26
Rohith150 <50076072+Rohith150@users.noreply.github.com>
2020-09-29 20:33:13 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.visibility = 225;
+  }
+
+  display(){
+
+    this.visibility = this.visibility - 10;
+
+    if(this.body.speed<4){
+      super.display();
+    }else{
+      push();
+      tint(225,this.visibility);
+      World.remove(world, this.body)
+      pop();
+    }
+
+  }
+
+};
